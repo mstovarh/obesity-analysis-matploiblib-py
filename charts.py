5047855cb47048660aee4010a2513698b8266805
Maria Stephanie Tovar <ingmariatovar@gmail.com>
2024-05-13 11:42:37 -0500

    Añadiendo libreria Pandas

--- charts.py
+++ charts.py
@@ -1,12 +1,16 @@
 import matplotlib.pyplot as plt
 
-def generate_bar_chart(labels, values, title, filename):
-    fig, ax = plt.subplots()
+def generate_bar_chart(labels, values, title, filename, fig_width=15, fig_height=10, font_size=12):
+    fig, ax = plt.subplots(figsize=(fig_width, fig_height))
     ax.bar(labels, values)
     ax.set_ylim(bottom=0)
     ax.set_title(title)
     ax.bar(labels, values, color=['#c0d88c', '#f7a472', '#f07877', '#fa2a3a', '#0a5c5a', '#680e34', '#fe4b74']) 
     #plt.show()
+
+    ax.tick_params(axis='x', labelsize = font_size)  
+    ax.tick_params(axis='y', labelsize = font_size)
+
     plt.savefig(f'./imgs/{filename}.png')
     plt.close()
 
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import read_csv
 import charts 
+import pandas as pd
 
-#Funcion de conteo por categorias
+#Funcion de conteo por categorias 
 def count_categories(data):
   weight_counts = {}
 
-  for item in data:
-    weight_category = item['NObeyesdad']
+  for index, row in data.iterrows():
+    weight_category = row['NObeyesdad']
     #print(weight_category)
     weight_counts[weight_category] = weight_counts.get(weight_category, 0) + 1
     #print(weight_counts)
@@ -17,28 +18,49 @@ def count_categories(data):
 
 #Funciones de conteo por categorias
 def get_Char1():
-  data = read_csv.read_csv('obesity_gen.csv')
+  '''data = read_csv.read_csv('obesity_gen.csv')
   data = list(filter(lambda item : item['Gender'] == 'Female', data))
   (weight_type, recuento) = count_categories(data)
+  charts.generate_bar_chart(weight_type, recuento, 'Estados de peso corporal en mujeres', 'chart1')'''
+
+  df = pd.read_csv('obesity_gen.csv', sep=';', header=0)
+  df = df[df['Gender'] == 'Female']
+  (weight_type, recuento) = count_categories(df)
   charts.generate_bar_chart(weight_type, recuento, 'Estados de peso corporal en mujeres', 'chart1')
 
+
 def get_Char2():
-  data = read_csv.read_csv('obesity_gen.csv')
+  """data = read_csv.read_csv('obesity_gen.csv')
   data = list(filter(lambda item : item['Gender'] == 'Male', data))
   (weight_type, recuento) = count_categories(data)
+  charts.generate_bar_chart(weight_type, recuento, 'Estados de peso corporal en hombres', 'chart2')"""
+
+  df = pd.read_csv('obesity_gen.csv', sep=';', header=0)
+  df = df[df['Gender'] == 'Male']
+  (weight_type, recuento) = count_categories(df)
   charts.generate_bar_chart(weight_type, recuento, 'Estados de peso corporal en hombres', 'chart2')
 
 def get_Char3():
-  data = read_csv.read_csv('obesity_gen.csv')
+  """data = read_csv.read_csv('obesity_gen.csv')
   data = list(filter(lambda item : item['Gender'] == 'Female' and (int(item['Age']) > 40 and int(item['Age']) < 60), data))
   (weight_type, recuento) = count_categories(data)
-  charts.generate_bar_chart(weight_type, recuento, 'Estados de peso corporal en mujeres entre 40 y 60 años', 'chart3')
+  charts.generate_bar_chart(weight_type, recuento, 'Estados de peso corporal en mujeres entre 40 y 60 años', 'chart3')"""
+
+  df = pd.read_csv('obesity_gen.csv', sep=';', header=0)
+  df = df[(df['Gender'] == 'Female') & ((df['Age'] > 40) & (df['Age'] < 60))]
+  (weight_type, recuento) = count_categories(df)
+  charts.generate_bar_chart(weight_type, recuento, 'Estados de peso corporal en mujeres entre 40 y 60 años', 'chart3')  
 
 def get_Char4():
-  data = read_csv.read_csv('obesity_gen.csv')
+  """data = read_csv.read_csv('obesity_gen.csv')
   data = list(filter(lambda item : item['Gender'] == 'Male' and (int(item['Age']) > 40 and int(item['Age']) < 60), data))
   (weight_type, recuento) = count_categories(data)
-  charts.generate_bar_chart(weight_type, recuento, 'Estados de peso corporal en hombres entre 40 y 60 años', 'chart4')
+  charts.generate_bar_chart(weight_type, recuento, 'Estados de peso corporal en hombres entre 40 y 60 años', 'chart4')"""
+
+  df = pd.read_csv('obesity_gen.csv', sep=';', header=0)
+  df = df[(df['Gender'] == 'Male') & ((df['Age'] > 40) & (df['Age'] < 60))]
+  (weight_type, recuento) = count_categories(df)
+  charts.generate_bar_chart(weight_type, recuento, 'Estados de peso corporal en hombres entre 40 y 60 años', 'chart4')  
 
 if __name__ == '__main__':
   get_Char1()
